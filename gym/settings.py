a3b9e65d3a02d6e603a07e801febd80599395287
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-10-18 13:53:40 +0100

    jkhgxcvnmn

--- gym/settings.py
+++ gym/settings.py
@@ -81,7 +81,6 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
@@ -99,7 +98,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
